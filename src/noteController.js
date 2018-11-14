ddb017739afd3b3d1c899ba51a5b91a3144d8e33
Caitlin Cooling <ccooling97@gmail.com>
2018-11-14 13:15:40 +0000

    noteController updates the document takes note text, note list object and note list view object creates the html for a note list view then finds the app id in the document and updates it

--- src/noteController.js
+++ src/noteController.js
@@ -1,7 +1,12 @@
-// note = new Note('test');
-// noteList = new NoteList;
-// noteList.addNote(note);
-// noteListView = new NoteListView(noteList.entries);
-// console.log(noteListView.returnList());
+(function(exports) {
+  function NoteController(noteText, noteList, noteListView) {
+    var note = new Note(noteText);
+    var noteList = new NoteList;
+    noteList.addNote(note.getText());
+    var noteListView = new NoteListView(noteList.entries);
+    document.getElementById('app').innerHTML = noteListView.returnList()
+  };
+  exports.NoteController = NoteController;
+})(this);
 
-document.getElementById('app').innerHTML = 'Howdy'
+new NoteController("Favourite drink: seltzer", NoteList, NoteListView)
