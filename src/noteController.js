17dcef5ad7bc3cb8bea4640a3c8fbcd55973dcd0
Caitlin Cooling <ccooling97@gmail.com>
2018-11-15 16:09:42 +0000

    Refactor: note controller

--- src/noteController.js
+++ src/noteController.js
@@ -3,10 +3,11 @@
     var note = new Note(noteText);
     var noteList = new NoteList;
     noteList.addNote(note.getText());
-    var noteListView = new NoteListView(noteList.entries);
-    this.updateText = function() {
-      document.getElementById('app').innerHTML = noteListView.returnList();
-    }
+    this.noteListView = new NoteListView(noteList.entries);
+  };
+
+  NoteController.prototype.updateText = function () {
+    document.getElementById('app').innerHTML = this.noteListView.returnList();
   };
   exports.NoteController = NoteController;
 })(this);
