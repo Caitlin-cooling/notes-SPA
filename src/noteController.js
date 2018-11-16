954ba079c798420b256dbdc1deda7df93eeefa30
Caitlin Cooling <ccooling97@gmail.com>
2018-11-16 10:56:43 +0000

    Note is rendered to the screen, submit value takes the value of the textarea

--- src/noteController.js
+++ src/noteController.js
@@ -4,33 +4,38 @@
   };
 
   NoteController.prototype.updateText = function () {
+    console.log(3)
     document.getElementById('app').innerHTML = this.noteListView.returnList();
   };
 
-  NoteController.prototype.renderNote = function () {
-    console.log("1")
-    document.getElementById('app').addEventListener("click", getNote);
-  };
-
   function getNote() {
-    console.log("2")
     getID();
-    console.log("4")
     document.getElementById('app').innerHTML = "Your note!"
   };
 
   function getID () {
-    console.log("3")
     return window.location.hash;
   };
-  
-  var button = document.getElementById('submit')
-  button.addEventListener('click',function(e) {
-    console.log(e);
-    e.preventDefault();
-    console.log(document.getElementById('note_text').value)
-  }, false);
 
+  NoteController.prototype.getSubmitValue = function(){
+    console.log(2)
+    var noteText = document.getElementById('note_text').value
+    var note = new Note(noteText);
+    var notelist = new NoteList;
+    notelist.addNote(note.text);
+    var noteListView = new NoteListView(noteList.entries);
+    var controller = new NoteController(noteListView);
+  };
+
+  NoteController.prototype.renderNote = function () {
+    console.log(1)
+    var button = document.getElementById('submit')
+    button.addEventListener('click',function(e) {
+      e.preventDefault();
+    }, false);
+    this.getSubmitValue();
+    this.updateText();
+  };
 
   exports.NoteController = NoteController;
 })(this);
